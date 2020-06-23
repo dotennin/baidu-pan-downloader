f283c73aefb0114d7739dcada0264c1243bc1696
nochi0105 <wenhua4438@gmail.com>
2020-06-23 17:23:19 +0900

    feat: auto extract comments

---
+++ src/baiduPanDownloader/UserScript.js
@@ -0,0 +1,29 @@
+module.exports = `
+// ==UserScript==
+// @namespace https://dotennin.blogspot.com/
+// @name 百度网盘下载管理器-alpha
+// @description A download manager for Baidu Yun
+// @version 0.2
+// @author Dotennin
+// @license MIT
+// @compatible        chrome/83.0.4103.97 passed
+// @compatible        edge/83.0.478.54 passed
+// @compatible        firefox untested
+// @compatible        opera untested
+// @compatible        safari untested
+// @include https://pan.baidu.com/disk/*
+// @connect baidu.com
+// @connect qdall01.baidupcs.com
+// @resource customStyle https://dotennin.github.io/baidu-disk-straight-chain/src/baiduPanDownloader/style.css
+// @grant GM_setClipboard
+// @grant GM_xmlhttpRequest
+// @grant GM_getResourceText
+// @grant GM_download
+// @grant GM_addStyle
+// @grant GM_notification
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_deleteValue
+// @run-at document-idle
+// ==/UserScript==
+`
