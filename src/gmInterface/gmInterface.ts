2ad541c9bbdb2e146fda785a33fcc7deb1634725
Dotennin <wenhua4438@gmail.com>
2020-06-24 02:23:49 +0900

    feat: gm interface

---
+++ src/gmInterface/gmInterface.ts
@@ -0,0 +1,237 @@
+/* eslint-disable @typescript-eslint/no-explicit-any,no-undef */
+import { Char, IDownloadDetail, INotification, IOpenInTabOption, IXMLHttpRequestDetail } from './types'
+
+/**
+ * This section describes how the Tampermonkey API can be used
+ * Application Programming Interface
+ * @link https://www.tampermonkey.net/documentation.php
+ */
+
+export const GM = {
+  /**
+   * Adds the given style to the document and returns the injected style element.
+   * @param css
+   */
+  addStyle: (css: HTMLStyleElement) => {
+    // @ts-ignore
+    GM_addStyle(css)
+  },
+
+  /**
+   * Deletes 'name' from storage.
+   * @param name
+   * @constructor
+   */
+  deleteValue: (name: string) => {
+    // @ts-ignore
+    GM_deleteValue(name)
+  },
+
+  /**
+   * List all names of the storage.
+   */
+  listValues: () => {
+    eval(`GM_listValues()`)
+  },
+
+  /**
+   * Adds a change listener to the storage and returns the listener ID.
+   * 'name' is the name of the observed variable.
+   * The 'remote' argument of the callback function shows whether this value was modified from the instance of another tab (true) or within this script instance (false).
+   * Therefore this functionality can be used by scripts of different browser tabs to communicate with each other.
+   * @param name
+   * @param callback
+   * @return number
+   */
+  addValueChangeListener: <N extends string>(
+    name: N,
+    callback: (name: N, oldValue: any, newValue: any, remote: boolean) => void
+  ): number => {
+    // @ts-ignore
+    return GM_addValueChangeListener(name, callback)
+  },
+
+  /**
+   * Removes a change listener by its ID.
+   * @param listenerId
+   */
+  removeValueChangeListener: (listenerId: number) => {
+    // @ts-ignore
+    GM_removeValueChangeListener(listenerId)
+  },
+
+  /**
+   * Set the value of 'name' to the storage.
+   * @param name
+   * @param value
+   * @constructor
+   */
+  setValue: (name: string, value: any) => {
+    // @ts-ignore
+    GM_setValue(name, value)
+  },
+
+  /**
+   * Get the value of 'name' from storage.
+   * @param name
+   * @param defaultValue
+   * @constructor
+   */
+  getValue: <V extends any>(name: string, defaultValue: V): V => {
+    // @ts-ignore
+    return GM_getValue(name, defaultValue)
+  },
+
+  /**
+   * Log a message to the console.
+   * @param message
+   */
+  log: (message: any) => {
+    // @ts-ignore
+    GM_log(message)
+  },
+
+  /**
+   * Get the content of a predefined @resource tag at the script header.
+   * @param name
+   */
+  getResourceText: (name: string): string => {
+    // @ts-ignore
+    return GM_getResourceText(name)
+  },
+
+  /**
+   * Get the base64 encoded URI of a predefined @resource tag at the script header.
+   * @param name
+   */
+  getResourceURL: (name: string): string => {
+    // @ts-ignore
+    return GM_getResourceURL(name)
+  },
+
+  /**
+   * Register a menu to be displayed at the Tampermonkey menu at pages where this script runs and returns a menu command ID.
+   * @param name
+   * @param callback
+   * @param accessKey
+   * @return number a menu command ID.
+   */
+  registerMenuCommand: (name: string, callback: Function, accessKey: Char): number => {
+    // @ts-ignore
+    return GM_registerMenuCommand(name, callback, accessKey)
+  },
+
+  /**
+   * Unregister a menu command that was previously registered by GM_registerMenuCommand with the given menu command ID.
+   * @param menuCmdId
+   */
+  unregisterMenuCommand: (menuCmdId: number) => {
+    // @ts-ignore
+    GM_unregisterMenuCommand(menuCmdId)
+  },
+
+  /**
+   * Open a new tab with this url. The options object can have the following properties:
+   * @param url
+   * @param loadInBackground
+   */
+  openInTab: (url: string, loadInBackground: boolean | IOpenInTabOption) => {
+    // @ts-ignore
+    GM_openInTab(url, loadInBackground)
+  },
+  /**
+   * Make an xmlHttpRequest.
+   * Property of details:
+   * @param details
+   */
+  xmlHttpRequest: (details: IXMLHttpRequestDetail): { abort: () => boolean } => {
+    // @ts-ignore
+    return GM_xmlhttpRequest(details)
+  },
+
+  /**
+   * Downloads a given URL to the local disk.
+   * @param urlOrdetails
+   * @param name
+   */
+  download: (urlOrdetails: string | IDownloadDetail, name?: string): { abort: () => boolean } => {
+    if (typeof urlOrdetails === 'string') {
+      // @ts-ignore
+      return GM_download(urlOrdetails, name)
+    }
+    // @ts-ignore
+    return GM_download(urlOrdetails)
+  },
+
+  /**
+   * Get a object that is persistent as long as this tab is open.
+   * @param callback
+   */
+  getTab: (callback: Function) => {
+    // @ts-ignore
+    GM_getTab(callback)
+  },
+
+  /**
+   * Save the tab object to reopen it after a page unload.
+   * @param tab
+   */
+  saveTab: (tab: number) => {
+    // @ts-ignore
+    GM_saveTab(tab)
+  },
+
+  /**
+   * Get all tab objects as a hash to communicate with other script instances.
+   * @param callback
+   */
+  getTabs: (callback: Function) => {
+    // @ts-ignore
+    GM_getTabs(callback)
+  },
+
+  /**
+   * Shows a HTML5 Desktop notification and/or highlight the current tab.
+   * @param textOrDetails
+   * @param titleOrOndone
+   * @param image
+   * @param onclick
+   */
+  notification: <T extends string | INotification>(
+    textOrDetails: T,
+    titleOrOndone: T extends string ? string : Function,
+    image?: string,
+    onclick?: Function
+  ) => {
+    if (typeof textOrDetails === 'string') {
+      // @ts-ignore
+      GM_notification(textOrDetails, titleOrOndone, image, onclick)
+    } else {
+      // @ts-ignore
+      GM_notification(textOrDetails, titleOrOndone)
+    }
+  },
+
+  /**
+   * Copies data into the clipboard. The parameter 'info' can be an object like "{ type: 'text', mimetype: 'text/plain'}" or just a string expressing the type ("text" or "html").
+   * @param data
+   * @param info
+   */
+  setClipboard: (
+    data: any,
+    info: { type: 'text' | 'html' | string; mimetype: 'text/plain' | string } = {
+      type: 'text',
+      mimetype: 'text/plain',
+    }
+  ) => {
+    // @ts-ignore
+    GM_setClipboard(data, info)
+  },
+
+  /**
+   * Get some info about the script and TM. The object might look like this:
+   */
+  // @ts-ignore
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  info: GM_info,
+}
---
+++ src/gmInterface/types.ts
@@ -0,0 +1,232 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type Char =
+  | 'a'
+  | 'b'
+  | 'c'
+  | 'd'
+  | 'e'
+  | 'f'
+  | 'g'
+  | 'h'
+  | 'i'
+  | 'j'
+  | 'k'
+  | 'l'
+  | 'm'
+  | 'n'
+  | 'o'
+  | 'p'
+  | 'q'
+  | 'r'
+  | 's'
+  | 't'
+  | 'u'
+  | 'v'
+  | 'w'
+  | 'x'
+  | 'y'
+  | 'z'
+  | 'A'
+  | 'B'
+  | 'C'
+  | 'D'
+  | 'E'
+  | 'F'
+  | 'G'
+  | 'H'
+  | 'I'
+  | 'J'
+  | 'K'
+  | 'L'
+  | 'M'
+  | 'N'
+  | 'O'
+  | 'P'
+  | 'Q'
+  | 'R'
+  | 'S'
+  | 'T'
+  | 'U'
+  | 'V'
+  | 'W'
+  | 'X'
+  | 'Y'
+  | 'Z'
+  | '0'
+  | '1'
+  | '2'
+  | '3'
+  | '4'
+  | '5'
+  | '6'
+  | '7'
+  | '8'
+  | '9'
+
+/**
+ * Otherwise the new tab is just appended. loadInBackground has the opposite meaning of active and was added to achieve Greasemonkey 3.x compatibility. If neither active nor loadInBackground is given, then the tab will not be focused. This function returns an object with the function close, the listener onclosed and a flag called closed.
+ */
+export interface IOpenInTabOption {
+  // decides whether the new tab should be focused,
+  active: boolean
+
+  // that inserts the new tab after the current one,
+  insert: boolean
+
+  // // makes the browser re-focus the current tab on close and
+  setParent: boolean
+
+  // makes the tab being opened inside a incognito mode/private mode window.
+  incognito: boolean
+}
+
+export interface IXMLHttpRequestDetail {
+  // one of GET, HEAD, POST
+  method: 'GET' | 'HEAD' | 'POST'
+
+  // the destination URL
+  url: string
+
+  // ie. user-agent, referer, ... (some special headers are not supported by Safari and Android browsers)
+  headers: string
+
+  // some string to send via a POST request
+  data: any
+
+  //  a cookie to be patched into the sent cookie set
+  cookie: string
+
+  // send the data string in binary mode
+  binary: boolean
+
+  //  don't cache the resource
+  nocache: boolean
+
+  // revalidate maybe cached content
+  revalidate: boolean | Date
+
+  // a timeout in ms
+  timeout: number
+
+  // a property which will be added to the response object
+  context: any
+
+  // one of arraybuffer, blob, json
+  responseType: 'arraybuffer' | 'blob' | 'json'
+
+  //  a MIME type for the request
+  overrideMimeType: MimeType
+
+  //  don't send cookies with the requests (please see the fetch notes)
+  anonymous: boolean
+
+  //  (beta) use a fetch instead of a xhr request
+  // (at Chrome this causes xhr.abort, details.timeout and xhr.onprogress to not work and makes xhr.onreadystatechange receive only readyState 4 events)
+  fetch: boolean
+
+  // a username for authentication
+  username: string
+
+  // a password
+  password: string
+
+  // callback to be executed if the request was aborted
+  onabort: (e: ProgressEvent) => void
+
+  // callback to be executed if the request ended up with an error
+  onerror: XMLHttpRequestEventTarget['onerror']
+
+  // callback to be executed if the request started to load
+  onloadstart: XMLHttpRequestEventTarget['onloadstart']
+
+  // callback to be executed if the request made some progress
+  onprogress: XMLHttpRequestEventTarget['onprogress']
+
+  // callback to be executed if the request's ready state changed
+  onreadystatechange: (e: Event) => void
+
+  // callback to be executed if the request failed due to a timeout
+  ontimeout: XMLHttpRequestEventTarget['ontimeout']
+
+  // callback to be executed if the request was loaded.
+  onload: (e: {
+    // the final URL after all redirects from where the data was loaded
+    finalUrl: string
+
+    // the ready state
+    readyState: 1 | 2 | 3 | 4
+
+    // the request status
+    status: number
+
+    // the request status text
+    statusText: string
+
+    // the request response headers
+    responseHeaders: string
+
+    // the response data as object if details.responseType was set
+    response: any
+
+    // the response data as XML document
+    responseXML: XMLDocument
+
+    // the response data as plain string
+    responseText: string
+  }) => void
+}
+
+export interface IDownloadDetail {
+  // the URL from where the data should be downloaded (required)
+  url: string
+
+  // the filename - for security reasons the file extension needs to be whitelisted at Tampermonkey's options page (required)
+  name: string
+
+  // - see GM_xmlhttpRequest for more details
+  headers: string
+
+  // boolean value, show a saveAs dialog
+  saveAs: boolean
+
+  // callback to be executed if this download ended up with an error
+  onerror: (e: {
+    error: 'not_enabled' | 'not_whitelisted' | 'not_permitted' | 'not_supported ' | 'not_succeeded'
+    details: any
+  }) => void
+
+  // callback to be executed if this download finished
+  onload: IXMLHttpRequestDetail['onload']
+
+  // callback to be executed if this download made some progress
+  onprogress: IXMLHttpRequestDetail['onprogress']
+
+  // callback to be executed if this download failed due to a timeout}
+  ontimeout: IXMLHttpRequestDetail['ontimeout']
+}
+
+export interface INotification {
+  // the text of the notification (required unless highlight is set)
+  text: string
+
+  // the notificaton title
+  title: string
+
+  // the image
+  image: string
+
+  // a boolean flag whether to highlight the tab that sends the notfication (required unless text is set)
+  highlight: boolean
+
+  // a boolean flag whether to not play a sound
+  silent: boolean
+
+  // the time after that the notification will be hidden (0 = disabled)
+  timeout: number
+
+  // called when the notification is closed (no matter if this was triggered by a timeout or a click) or the tab was highlighted
+  ondone: Function
+
+  // called in case the user clicks the notification
+  onclick: Function
+}
