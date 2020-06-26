2e99f7718484848307ea86be6319b33d219be2de
nochi0105 <wenhua4438@gmail.com>
2020-06-27 01:27:40 +0900

    chore: output file bundle.js to user.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   mode: 'development',
   entry: './src/index.ts',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.user.js',
     path: resolve(__dirname, 'public'),
   },
   resolve: {
