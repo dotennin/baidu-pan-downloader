a7c8cc536b7e18b57554c6bb5f1500229ce15c4a
Dotennin <wenhua4438@gmail.com>
2020-07-13 18:08:55 +0900

    fix: type check

--- src/containers/DownloadList/Operation.tsx
+++ src/containers/DownloadList/Operation.tsx
@@ -10,7 +10,7 @@ interface IProps {
   fsId: ItemProxy['fsId']
 }
 const mapStoreToProps = (store: IStoreState, props: IProps) => ({
-  status: store.download.downloadItems[props.fsId]!.status,
+  status: store.download.downloadItems[props.fsId]?.status,
 })
 
 function Operation({ fsId, status }: ReturnType<typeof mapStoreToProps> & IProps) {
@@ -25,7 +25,7 @@ function Operation({ fsId, status }: ReturnType<typeof mapStoreToProps> & IProps
         dispatch(downloadModule.actions.failureDownload())
       }
       targetItem.progress.status = StatusTypes.stopped
-      targetItem.progress.request!.abort && targetItem.progress.request!.abort()
+      targetItem.progress.request?.abort && targetItem.progress.request?.abort()
       clearInterval(targetItem.progress.intervalId)
 
       dispatch(addNextDownloadRequest())
