cda3b585552c6b57035f7c47b33359ea9d0d53a9
Dotennin <wenhua4438@gmail.com>
2020-07-13 16:16:26 +0900

    fix: missing style from styled-components

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components/macro'
 
 const GlobalStyle = createGlobalStyle`
   @import url(https://fonts.googleapis.com/css?family=Noto+Sans);
--- src/components/Form.ts
+++ src/components/Form.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
 
 const FormField = styled.fieldset`
   clear: both;
